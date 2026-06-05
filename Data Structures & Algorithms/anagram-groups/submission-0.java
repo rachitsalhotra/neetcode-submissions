2d08acfbb9180e069ac2d33b3e89e447a1fd9622
Rachit Salhotra <79327585+rachitsalhotra@users.noreply.github.com>
2026-06-05 20:24:30 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            int count[] = new int[26];
+            for (int j = 0; j < strs[i].length(); j++) {
+                char c = strs[i].charAt(j);
+                count[c - 'a']++;
+            }
+
+            String key = Arrays.toString(count);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(strs[i]);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num: nums) {
+            set.add(num);
+        }
+
+        return nums.length != set.size();
+    }
+}
