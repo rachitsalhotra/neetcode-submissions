79149df80ebb46509fd6f99bcbd1aaf67fedc7a6
Rachit Salhotra <79327585+rachitsalhotra@users.noreply.github.com>
2026-06-07 01:34:10 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    private boolean isPalindromeHelper(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i <= j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+                sb.append(c);
+            }
+        }
+        return isPalindromeHelper(sb.toString().toLowerCase());
+    }
+}
