bbeb81be54392d7f3a31c2fb81fd17e9e65d32e5
Rachit Salhotra <79327585+rachitsalhotra@users.noreply.github.com>
2026-08-17 12:54:26 +0530

    Add: design-word-search-data-structure - submission-4

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-4.java
@@ -0,0 +1,47 @@
+public class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean endOfWord = false;
+}
+class WordDictionary {
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode parent = root;
+        for (char c: word.toCharArray()) {
+            int i = c - 'a';
+            if (parent.children[i] == null) {
+                parent.children[i] = new TrieNode();
+            }
+            parent = parent.children[i];
+        }
+        parent.endOfWord = true;
+    }
+
+    private boolean search(String word, TrieNode parent) {
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c == '.') {
+                for (TrieNode child: parent.children) {
+                    if (child != null && search(word.substring(i + 1), child)) {
+                        return true;
+                    }
+                }
+                return false;
+            } else {
+                if (parent.children[c - 'a'] == null) {
+                    return false;
+                }
+                parent = parent.children[c - 'a'];
+            }
+        }
+        return parent.endOfWord;
+    }
+
+    public boolean search(String word) {
+        return search(word, root);
+    }
+}
