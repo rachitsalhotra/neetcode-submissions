92fa564e760c69636319f8c433572269c96e27ff
Rachit Salhotra <79327585+rachitsalhotra@users.noreply.github.com>
2026-08-22 13:11:36 +0530

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.java
@@ -0,0 +1,54 @@
+class Solution {
+    private boolean rottingComplete(int[][] grid, int m, int n) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public int orangesRotting(int[][] grid) {
+        Queue<int[]> q = new LinkedList<>();
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    q.add(new int[] {i, j});
+                }
+            }
+        }
+        if (q.isEmpty()) {
+            return rottingComplete(grid, m, n) ? 0 : -1;
+        }
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int minute = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            minute++;
+            for (int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+                int r = curr[0];
+                int c = curr[1];
+                grid[r][c] = 2;
+                for (int[] dir: directions) {
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
+                    if (nr < 0 || nc < 0 || nr >= m || nc >= n || grid[nr][nc] == 0 || grid[nr][nc] == 2) {
+                        continue;
+                    }
+                    q.add(new int[] {nr, nc});
+                }
+            }
+            if (rottingComplete(grid, m, n)) {
+                return minute - 1;
+            }
+        }
+        if (rottingComplete(grid, m, n)) {
+            return minute - 1;
+        }
+        return -1;
+    }
+}
