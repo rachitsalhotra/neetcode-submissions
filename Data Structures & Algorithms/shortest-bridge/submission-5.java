cf29241a4876274dca48fb91d5a759ca0d681802
Rachit Salhotra <79327585+rachitsalhotra@users.noreply.github.com>
2026-08-27 01:27:35 +0530

    Add: shortest-bridge - submission-5

---
+++ Data Structures & Algorithms/shortest-bridge/submission-5.java
@@ -0,0 +1,59 @@
+class Solution {
+    private static final int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private void dfs(int r, int c, int[][] grid) {
+        grid[r][c] = 2;
+        for (int[] dir: directions) {
+            int nr = r + dir[0];
+            int nc = c + dir[1];
+
+            if (nr >= 0 && nc >= 0 && nr < grid.length && nc < grid[0].length && grid[nr][nc] == 1) {
+                dfs(nr, nc, grid);
+            }
+        }
+    }
+    public int shortestBridge(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean flag = false;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    dfs(i, j, grid);
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    q.add(new int[] {i, j, 0});
+                    grid[i][j] = 2;
+                }
+            }
+        }
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int[] node = q.poll();
+                for (int[] dir: directions) {
+                    int r = node[0] + dir[0];
+                    int c = node[1] + dir[1];
+                    if (r >= 0 && c >= 0 && r < grid.length && c < grid[0].length && grid[r][c] != 2) {
+                        if (grid[r][c] == 1) {
+                            return node[2];
+                        }
+                        grid[r][c] = 2;
+                        q.add(new int[] {r, c, node[2] + 1});
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
