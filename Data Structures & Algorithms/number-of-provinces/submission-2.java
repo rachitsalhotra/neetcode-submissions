4e943f4a3b22fc88d293698e48a59692a0ecde2d
Rachit Salhotra <79327585+rachitsalhotra@users.noreply.github.com>
2026-09-08 16:35:50 +0530

    Add: number-of-provinces - submission-2

---
+++ Data Structures & Algorithms/number-of-provinces/submission-2.java
@@ -0,0 +1,56 @@
+class DSU {
+    int[] Parent, Size;
+    int components;
+
+    public DSU(int n) {
+        Parent = new int[n];
+        Size = new int[n];
+        components = n;
+        for (int i = 0; i < n; i++) {
+            Parent[i] = i;
+            Size[i] = 1;
+        }
+    }
+
+    public int find(int node) {
+        if (Parent[node] != node) {
+            Parent[node] = find(Parent[node]);
+        }
+        return Parent[node];
+    }
+
+    public boolean union(int u, int v) {
+        int pu = find(u), pv = find(v);
+        if (pu == pv) return false;
+
+        components--;
+        if (Size[pu] >= Size[pv]) {
+            Size[pu] += Size[pv];
+            Parent[pv] = pu;
+        } else {
+            Size[pv] += Size[pu];
+            Parent[pu] = pv;
+        }
+        return true;
+    }
+
+    public int numOfComps() {
+        return components;
+    }
+}
+
+public class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        DSU dsu = new DSU(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    dsu.union(i, j);
+                }
+            }
+        }
+        return dsu.numOfComps();
+    }
+}
