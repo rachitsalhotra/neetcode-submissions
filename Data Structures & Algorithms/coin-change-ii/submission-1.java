90dae814064a95c8fc578b15313c56fa31baf12a
Rachit Salhotra <79327585+rachitsalhotra@users.noreply.github.com>
2026-09-08 12:22:06 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    private int[][] mem;
+    private int change(int amount, int[] coins, int si) {
+        if (amount == 0) {
+            return 1;
+        }
+        if (si >= coins.length) {
+            return 0;
+        }
+        if (mem[si][amount] != -1) {
+            return mem[si][amount];
+        }
+        int res = 0;
+        for (int i = si; i < coins.length; i++) {
+            int newAmount = amount - coins[i];
+            if (newAmount < 0) {
+                continue;
+            }
+            res += change(newAmount, coins, i);
+        }
+        mem[si][amount] = res;
+        return res;
+    }
+    public int change(int amount, int[] coins) {
+        mem = new int[coins.length][amount + 1];
+        for (int[] row: mem) {
+            Arrays.fill(row, -1);
+        }
+        return change(amount, coins, 0);
+    }
+}
