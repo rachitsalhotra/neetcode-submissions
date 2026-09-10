3775e148e7f16d04dc748949a31221189dcafd45
Rachit Salhotra <79327585+rachitsalhotra@users.noreply.github.com>
2026-09-10 16:21:27 +0530

    Add: stone-game - submission-9

---
+++ Data Structures & Algorithms/stone-game/submission-9.java
@@ -0,0 +1,29 @@
+class Solution {
+    private int[][] mem;
+    private int stoneGame(int[] piles, int i, int j, boolean aliceTurn) {
+        if (i > j) {
+            return 0;
+        }
+        if (mem[i][j] != -1) {
+            return mem[i][j];
+        }
+        int sum1 = stoneGame(piles, i + 1, j, !aliceTurn);
+        int sum2 = stoneGame(piles, i, j - 1, !aliceTurn);
+        if (aliceTurn) {
+            sum1 += piles[i];
+            sum2 += piles[j];
+        }
+        mem[i][j] = Math.max(sum1, sum2);
+        return mem[i][j];
+    }
+    public boolean stoneGame(int[] piles) {
+        mem = new int[piles.length][piles.length];
+        for (int[] row: mem) {
+            Arrays.fill(row, -1);
+        }
+        int sum = Arrays.stream(piles).sum();
+        int alice = stoneGame(piles, 0, piles.length - 1, true);
+        int bob = sum - alice;
+        return alice > bob ? true : false;
+    }
+}
